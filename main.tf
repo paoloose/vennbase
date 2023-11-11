dc3c64cacea966db66d969a37e51a0ceefe1d4c9
paoloose <pflores.fisi22@gmail.com>
2023-11-11 09:56:32 -0500

    add terraform base config

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+}
+
+resource "azurerm_resource_group" "vennbase-rg" {
+  name     = "vennbase-resources"
+  location = "East US"
+  tags = {
+    environments = "dev"
+  }
+}
